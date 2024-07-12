06644fce0d8aa3ed2ef7a7e22b0895fdbb4bcb57
Md. Shihab hossen <shihabhossen7463@gmail.com>
2024-07-13 00:00:41 +0600

    Add files via upload

---
+++ Pattern.php
@@ -0,0 +1,27 @@
+<?php
+
+
+$rows = 4;
+
+for ($i = 1; $i <= $rows; $i++) {
+  for ($j = $i; $j < $rows; $j++) {
+    echo "&nbsp;&nbsp;&nbsp";
+  }
+  for ($j = 0; $j <= (2 * $i - 2); $j++) {
+    echo "* ";
+  }
+  echo "<br>";
+}
+
+echo "<br>";
+echo "<br>";
+
+
+for ($i = 0; $i < 5; $i++) {
+
+  for ($j = 0; $j <= 7; $j++) {
+    echo "*";
+  }
+  echo "<br>";}
+
+?>
